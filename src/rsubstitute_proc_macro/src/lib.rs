3b4ddec533b174b9e7bfe641f6e0131800495b8d
alordash <alordash@mail.ru>
2025-12-07 23:05:26 +0100

    Init

---
+++ src/rsubstitute_proc_macro/src/lib.rs
@@ -0,0 +1,7 @@
+#[proc_macro_attribute]
+pub fn mock(
+    proc_macro_attribute: proc_macro::TokenStream,
+    proc_macro_item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    todo!()
+}
