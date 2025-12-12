d1d2612b60df0e6eb0018b914af7f6d27ce1b486
alordash <alordash1@gmail.com>
2025-12-12 18:20:03 +0100

    Reorder args

--- src/rsubstitute_core/src/arguments_matching/arg.rs
+++ src/rsubstitute_core/src/arguments_matching/arg.rs
@@ -1,10 +1,9 @@
-use std::ops::Deref;
 use std::sync::Arc;
 
 pub enum Arg<T> {
     Any,
-    Is(fn(T) -> bool),
     Eq(T),
+    Is(fn(T) -> bool),
 }
 
 trait Quo {}
@@ -13,8 +12,8 @@ impl<T: PartialOrd> Arg<T> {
     pub fn matches(&self, value: T) -> bool {
         match self {
             Arg::Any => true,
-            Arg::Is(predicate) => predicate(value),
             Arg::Eq(expected_value) => value.eq(expected_value),
+            Arg::Is(predicate) => predicate(value),
         }
     }
 }
@@ -23,8 +22,8 @@ impl<'a, T: ?Sized> Arg<&'a T> {
     pub fn matches_ref(&self, value: &'a T) -> bool {
         match self {
             Arg::Any => true,
-            Arg::Is(predicate) => predicate(value),
             Arg::Eq(expected_value) => std::ptr::eq(*expected_value, value),
+            Arg::Is(predicate) => predicate(value),
         }
     }
 }
@@ -33,8 +32,8 @@ impl<T: ?Sized> Arg<Arc<T>> {
     pub fn matches_arc(&self, value: Arc<T>) -> bool {
         match self {
             Arg::Any => true,
-            Arg::Is(predicate) => predicate(value),
             Arg::Eq(expected_value) => Arc::ptr_eq(expected_value, &value),
+            Arg::Is(predicate) => predicate(value),
         }
     }
 }
