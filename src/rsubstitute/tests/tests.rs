5e1cdc3ebce75f45e141d960f5acfd978d6b3128
alordash <alordash1@gmail.com>
2025-12-18 18:55:57 +0400

    Handling various arg types

--- src/rsubstitute/tests/tests.rs
+++ src/rsubstitute/tests/tests.rs
@@ -1,24 +1,53 @@
 use rsubstitute_proc_macro::mock;
+use std::rc::Rc;
+use std::sync::Arc;
 
 #[mock]
 trait Foo {
-    fn work(&self, number: i32) -> f32;
+    fn work(&self, number: i32, string: String, rc: Rc<u8>, arc: Arc<u8>, r#box: Box<u8>) -> f32;
 }
 
 mod tests {
+    use super::__rsubstitute_generated::FooMock;
+    use crate::Foo;
     use rsubstitute::Times;
     use rsubstitute::prelude::Arg;
-    use crate::Foo;
-    use super::__rsubstitute_generated::FooMock;
+    use std::rc::Rc;
+    use std::sync::Arc;
 
     #[test]
     fn a() {
         let foo_mock = FooMock::new();
-        foo_mock.work(Arg::Any).returns(21.0f32);
-        
-        let result = Foo::work(&foo_mock, 20);
-        
+        let rc = Rc::new(1);
+        let arc = Arc::new(2);
+        let r#box = Box::new(3);
+        foo_mock
+            .work(
+                Arg::Any,
+                Arg::Eq(String::from("amogus")),
+                Arg::Any,
+                Arg::Any,
+                Arg::Any,
+            )
+            .returns(21.0f32);
+
+        let result = Foo::work(
+            &foo_mock,
+            20,
+            String::from("amogus"),
+            rc.clone(),
+            arc.clone(),
+            r#box,
+        );
+
         assert_eq!(21.0f32, result);
-        foo_mock.work_received(Arg::Eq(20), Times::Once);
+        foo_mock.work_received(
+            Arg::Eq(20),
+            Arg::Is(|x| x == "amogus"),
+            Arg::Eq(rc),
+            Arg::Eq(arc),
+            Arg::Eq(Box::new(3)),
+            Times::Once,
+        );
     }
 }
