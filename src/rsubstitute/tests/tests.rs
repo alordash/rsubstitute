5371224eec0fc4f9ab7b9d13d79b69e81eedbe1e
alordash <alordash1@gmail.com>
2025-12-18 14:04:13 +0400

    Testing mock

--- src/rsubstitute/tests/tests.rs
+++ src/rsubstitute/tests/tests.rs
@@ -6,11 +6,19 @@ trait Foo {
 }
 
 mod tests {
+    use rsubstitute::Times;
+    use rsubstitute::prelude::Arg;
     use crate::Foo;
     use super::__rsubstitute_generated::FooMock;
-    
+
+    #[test]
     fn a() {
         let foo_mock = FooMock::new();
-        // foo_mock.work(rsubstitute::prelude::Arg::Any);
+        foo_mock.work(Arg::Any).returns(21.0f32);
+        
+        let result = Foo::work(&foo_mock, 20);
+        
+        assert_eq!(21.0f32, result);
+        foo_mock.work_received(Arg::Eq(20), Times::Once);
     }
 }
