eed4b2783a686420861e7a390f6869601be9173e
alordash <alordash1@gmail.com>
2026-01-23 15:19:49 +0100

    Adding more generic tests

--- src/rsubstitute/tests/fn_generics_tests.rs
+++ src/rsubstitute/tests/fn_generics_tests.rs
@@ -5,6 +5,11 @@ fn get_return<T>(value: T) -> T {
     return value;
 }
 
+#[mock]
+fn return_constraint<T: Default>() -> T {
+    return T::default();
+}
+
 mod tests {
     #![allow(unused_variables)]
     #![allow(non_snake_case)]
@@ -12,60 +17,104 @@ mod tests {
     use rsubstitute::assertions::assert_panics;
     use rsubstitute::*;
 
-    #[test]
-    fn get_return_Ok() {
-        // Arrange
-        let accepted_number = 10;
-        let returned_number = 20;
-        get_return::setup(accepted_number).returns(returned_number);
+    mod get_return_tests {
+        use super::*;
 
-        // Act
-        let actual_returned_number = get_return(accepted_number);
+        #[test]
+        fn get_return_Ok() {
+            // Arrange
+            let accepted_number = 10;
+            let returned_number = 20;
+            get_return::setup(accepted_number).returns(returned_number);
 
-        // Assert
-        assert_eq!(returned_number, actual_returned_number);
-        get_return::received(accepted_number, Times::Once).no_other_calls();
+            // Act
+            let actual_returned_number = get_return(accepted_number);
+
+            // Assert
+            assert_eq!(returned_number, actual_returned_number);
+            get_return::received(accepted_number, Times::Once).no_other_calls();
+        }
+
+        #[test]
+        fn get_return_TwoTypes_Ok() {
+            // Arrange
+            let accepted_number = 10;
+            let returned_number = 20;
+            get_return::setup(accepted_number).returns(returned_number);
+
+            let accepted_str = "quo vadis";
+            let returned_str = "veridis quo";
+            get_return::setup(accepted_str).returns(returned_str);
+
+            // Act
+            let actual_returned_number = get_return(accepted_number);
+            let actual_returned_str = get_return(accepted_str);
+
+            // Assert
+            assert_eq!(returned_number, actual_returned_number);
+            get_return::received(accepted_number, Times::Once).no_other_calls();
+
+            assert_eq!(returned_str, actual_returned_str);
+            get_return::received(accepted_str, Times::Once).no_other_calls();
+        }
+
+        #[test]
+        fn get_return_UnconfiguredType_PanicsOk() {
+            // Arrange
+            let accepted_number = 10;
+            let returned_number = 20;
+            get_return::setup(accepted_number).returns(returned_number);
+
+            // Act
+            let actual_returned_number = get_return(accepted_number);
+            assert_panics(
+                || get_return("str wasn't configured"),
+                "No fn configuration found for this call! TODO: write call description",
+            );
+
+            // Assert
+            assert_eq!(returned_number, actual_returned_number);
+            get_return::received(accepted_number, Times::Once).no_other_calls();
+        }
+
+        #[test]
+        fn get_return_CallBase_Ok() {
+            // Arrange
+            let accepted_number = 33;
+            get_return::setup(accepted_number).call_base();
+
+            // Act
+            let returned_number = get_return(accepted_number);
+
+            // Assert
+            assert_eq!(accepted_number, returned_number);
+            get_return::received(accepted_number, Times::Once).no_other_calls();
+        }
     }
 
-    #[test]
-    fn get_return_TwoTypes_Ok() {
-        // Arrange
-        let accepted_number = 10;
-        let returned_number = 20;
-        get_return::setup(accepted_number).returns(returned_number);
+    mod return_constraint_tests {
+        use super::*;
 
-        let accepted_str = "quo vadis";
-        let returned_str = "veridis quo";
-        get_return::setup(accepted_str).returns(returned_str);
+        #[test]
+        fn return_constraint_Ok() {
+            // Arrange
+            let returned_number_value = 100;
+            return_constraint::setup().returns(returned_number_value);
+            let returned_string_value = String::from("quo vadis");
+            return_constraint::setup().returns(returned_string_value.clone());
 
-        // Act
-        let actual_returned_number = get_return(accepted_number);
-        let actual_returned_str = get_return(accepted_str);
+            // Act
+            let actual_number_value = return_constraint();
+            let actual_string_value: String = return_constraint();
 
-        // Assert
-        assert_eq!(returned_number, actual_returned_number);
-        get_return::received(accepted_number, Times::Once).no_other_calls();
+            // Assert
+            assert_eq!(returned_number_value, actual_number_value);
+            return_constraint::received::<i32>(Times::Once).no_other_calls();
 
-        assert_eq!(returned_str, actual_returned_str);
-        get_return::received(accepted_str, Times::Once).no_other_calls();
-    }
+            assert_eq!(returned_string_value, actual_string_value);
+            return_constraint::received::<String>(Times::Once).no_other_calls();
 
-    #[test]
-    fn get_return_UnconfiguredType_PanicsOk() {
-        // Arrange
-        let accepted_number = 10;
-        let returned_number = 20;
-        get_return::setup(accepted_number).returns(returned_number);
-
-        // Act
-        let actual_returned_number = get_return(accepted_number);
-        assert_panics(
-            || get_return("str wasn't configured"),
-            "No fn configuration found for this call! TODO: write call description",
-        );
-
-        // Assert
-        assert_eq!(returned_number, actual_returned_number);
-        get_return::received(accepted_number, Times::Once).no_other_calls();
+            return_constraint::received::<()>(Times::Never).no_other_calls();
+        }
     }
 }
--- src/rsubstitute/tests/trait_generics_tests.rs
+++ src/rsubstitute/tests/trait_generics_tests.rs
@@ -2,9 +2,9 @@ use rsubstitute::macros::mock;
 
 #[mock]
 trait Trait<T1, T2> {
-    fn work(&self, value: T1) -> T1;
+    fn get_return(&self, value: T1) -> T1;
 
-    fn rest(&self, value: T1) -> T2;
+    fn get_return_different(&self, value: T1) -> T2;
 }
 
 mod trait_generic_tests {
@@ -13,88 +13,102 @@ mod trait_generic_tests {
     use super::*;
     use rsubstitute::*;
 
-    #[test]
-    fn work_first_type_i32_Ok() {
-        // Arrange
-        let mock = TraitMock::<_, String>::new();
-        let accepted_value = 10;
-        let returned_value = 200;
-        mock.setup.work(accepted_value).returns(returned_value);
+    mod get_return_tests {
+        use super::*;
 
-        // Act
-        let actual_returned_value = mock.work(accepted_value);
+        #[test]
+        fn get_return_first_type_i32_Ok() {
+            // Arrange
+            let mock = TraitMock::<_, String>::new();
+            let accepted_value = 10;
+            let returned_value = 200;
+            mock.setup
+                .get_return(accepted_value)
+                .returns(returned_value);
 
-        // Assert
-        assert_eq!(returned_value, actual_returned_value);
+            // Act
+            let actual_returned_value = mock.get_return(accepted_value);
 
-        mock.received
-            .work(accepted_value, Times::Once)
-            .no_other_calls();
+            // Assert
+            assert_eq!(returned_value, actual_returned_value);
+
+            mock.received
+                .get_return(accepted_value, Times::Once)
+                .no_other_calls();
+        }
+
+        #[test]
+        fn get_return_first_type_str_Ok() {
+            // Arrange
+            let mock = TraitMock::<_, ()>::new();
+            let accepted_value = "quo vadis";
+            let returned_value = "veridis quo";
+            mock.setup
+                .get_return(accepted_value)
+                .returns(returned_value);
+
+            // Act
+            let actual_returned_value = mock.get_return(accepted_value);
+
+            // Assert
+            assert_eq!(returned_value, actual_returned_value);
+
+            mock.received
+                .get_return(accepted_value, Times::Once)
+                .no_other_calls();
+        }
+
+        #[test]
+        fn get_return_first_type_explicit_type_Ok() {
+            // Arrange
+            let mock = TraitMock::<u8, Box<Vec<String>>>::new();
+            let accepted_value = 10u8;
+            let returned_value = 20u8;
+            mock.setup
+                .get_return(accepted_value)
+                .returns(returned_value);
+
+            // Act
+            let actual_returned_value = mock.get_return(accepted_value);
+
+            // Assert
+            assert_eq!(returned_value, actual_returned_value);
+
+            mock.received
+                .get_return(accepted_value, Times::Once)
+                .no_other_calls();
+        }
     }
 
     #[test]
-    fn work_first_type_str_Ok() {
-        // Arrange
-        let mock = TraitMock::<_, ()>::new();
-        let accepted_value = "quo vadis";
-        let returned_value = "veridis quo";
-        mock.setup.work(accepted_value).returns(returned_value);
-
-        // Act
-        let actual_returned_value = mock.work(accepted_value);
-
-        // Assert
-        assert_eq!(returned_value, actual_returned_value);
-
-        mock.received
-            .work(accepted_value, Times::Once)
-            .no_other_calls();
-    }
-
-    #[test]
-    fn work_first_type_explicit_type_Ok() {
-        // Arrange
-        let mock = TraitMock::<u8, Box<Vec<String>>>::new();
-        let accepted_value = 10u8;
-        let returned_value = 20u8;
-        mock.setup.work(accepted_value).returns(returned_value);
-
-        // Act
-        let actual_returned_value = mock.work(accepted_value);
-
-        // Assert
-        assert_eq!(returned_value, actual_returned_value);
-
-        mock.received
-            .work(accepted_value, Times::Once)
-            .no_other_calls();
-    }
-
-    #[test]
-    fn work_and_rest_work_Ok() {
+    fn get_return_and_get_return_different_get_return_Ok() {
         // Arrange
         let mock = TraitMock::new();
-        let accepted_work_value = 10;
-        let returned_work_value = 20;
-        let accepted_rest_value = 15;
-        let returned_rest_value = "quo vadis";
+        let accepted_get_return_value = 10;
+        let returned_get_return_value = 20;
+        let accepted_get_return_different_value = 15;
+        let returned_get_return_different_value = "quo vadis";
         mock.setup
-            .work(accepted_work_value)
-            .returns(returned_work_value)
-            .rest(accepted_rest_value)
-            .returns(returned_rest_value);
+            .get_return(accepted_get_return_value)
+            .returns(returned_get_return_value)
+            .get_return_different(accepted_get_return_different_value)
+            .returns(returned_get_return_different_value);
 
         // Act
-        let actual_returned_work_value = mock.work(accepted_work_value);
-        let actual_returned_rest_value = mock.rest(accepted_rest_value);
+        let actual_returned_get_return_value = mock.get_return(accepted_get_return_value);
+        let actual_returned_get_return_different_value =
+            mock.get_return_different(accepted_get_return_different_value);
 
         // Assert
-        assert_eq!(returned_work_value, actual_returned_work_value);
-        assert_eq!(returned_rest_value, actual_returned_rest_value);
+        assert_eq!(returned_get_return_value, actual_returned_get_return_value);
+        assert_eq!(
+            returned_get_return_different_value,
+            actual_returned_get_return_different_value
+        );
 
         mock.received
-            .work(accepted_work_value, Times::Once)
-            .rest(accepted_rest_value, Times::Once)
+            .get_return(accepted_get_return_value, Times::Once)
+            .get_return_different(accepted_get_return_different_value, Times::Once)
             .no_other_calls();
     }
 }
