88bf0cd551d07ed661dd0ff46f17f96d3f86991b
alordash <alordash@mail.ru>
2026-02-11 23:50:25 +0400

    somethign strange

--- src/rsubstitute/tests/trait_advanced_ref_tests.rs
+++ src/rsubstitute/tests/trait_advanced_ref_tests.rs
@@ -5,7 +5,6 @@ struct Foo {
     pub number: Vec<i32>,
 }
 
-#[mock]
 trait Trait<'a, 'b> {
     // fn accept_ref(&self, r: &'a &'b &'a &i32) -> &'b &'a &'b &'a i32;
 
@@ -15,6 +14,206 @@ trait Trait<'a, 'b> {
         mut qq: &'d mut &'b mut &'a &'a &'c &'d mut i32,
     ) {
         println!("number: {number:?}")
+    }
+}
+#[cfg(test)]
+pub use __rsubstitute_generated_Trait::*;
+#[cfg(test)]
+#[allow(dead_code)]
+#[allow(unused)]
+#[allow(non_snake_case)]
+#[allow(non_camel_case_types)]
+mod __rsubstitute_generated_Trait {
+    use super::*;
+    use rsubstitute::for_generated::*;
+    #[doc(hidden)]
+    #[derive(Clone)]
+    pub struct fooo_Call<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a> {
+        _phantom_lifetime: PhantomData<&'__rsubstitute_arg_field_lifetime ()>,
+        _phantom_a: PhantomData<&'a ()>,
+        _phantom_b: PhantomData<&'b ()>,
+        arg_1: Foo,
+        qq: *mut &'b mut &'a &'a &'c &'d mut i32,
+    }
+    impl<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a> IArgInfosProvider
+        for fooo_Call<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>
+    {
+        fn get_arg_infos(&self) -> Vec<ArgInfo> {
+            vec![
+                ArgInfo::new("arg_1", self.arg_1.clone()),
+                ArgInfo::new("qq", self.qq.clone()),
+            ]
+        }
+    }
+    #[doc(hidden)]
+    #[derive(Debug, IArgsFormatter)]
+    pub struct fooo_ArgsChecker<
+        '__rsubstitute_arg_field_lifetime: 'a + 'b,
+        'a,
+        'b,
+        'c: 'a,
+        'd: 'b + 'a,
+    > {
+        _phantom_lifetime: PhantomData<&'__rsubstitute_arg_field_lifetime ()>,
+        _phantom_a: PhantomData<&'a ()>,
+        _phantom_b: PhantomData<&'b ()>,
+        arg_1: Arg<Foo>,
+        qq: Arg<&'d mut &'b mut &'a &'a &'c &'d mut i32>,
+    }
+    impl<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a>
+        IArgsChecker<fooo_Call<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>>
+        for fooo_ArgsChecker<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>
+    {
+        fn check(
+            &self,
+            call: &fooo_Call<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+        ) -> Vec<ArgCheckResult> {
+            vec![
+                self.arg_1.check("arg_1", &call.arg_1),
+                self.qq.check_mut("qq", &call.qq),
+            ]
+        }
+    }
+    impl<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a>
+        IBaseCaller<fooo_Call<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>, ()>
+        for TraitMock<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>
+    {
+        fn call_base(&self, call: fooo_Call<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>) {
+            let fooo_Call {
+                arg_1: Foo { mut number },
+                qq: mut qq,
+                ..
+            } = call;
+            println!("number: {number:?}")
+        }
+    }
+    #[doc(hidden)]
+    #[derive(IMockData)]
+    pub struct TraitMockData<
+        '__rsubstitute_arg_field_lifetime: 'a + 'b,
+        'a,
+        'b,
+        'c: 'a,
+        'd: 'b + 'a,
+    > {
+        _phantom_lifetime: PhantomData<&'__rsubstitute_arg_field_lifetime ()>,
+        fooo_data: FnData<
+            TraitMock<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+            fooo_Call<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+            fooo_ArgsChecker<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+            (),
+        >,
+    }
+    #[doc(hidden)]
+    pub struct TraitMockSetup<
+        '__rsubstitute_arg_field_lifetime: 'a + 'b,
+        'a,
+        'b,
+        'c: 'a,
+        'd: 'b + 'a,
+    > {
+        data: Arc<TraitMockData<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>>,
+    }
+    #[doc(hidden)]
+    pub struct TraitMockReceived<
+        '__rsubstitute_arg_field_lifetime: 'a + 'b,
+        'a,
+        'b,
+        'c: 'a,
+        'd: 'b + 'a,
+    > {
+        data: Arc<TraitMockData<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>>,
+    }
+    pub struct TraitMock<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a> {
+        pub setup: TraitMockSetup<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+        pub received: TraitMockReceived<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+        data: Arc<TraitMockData<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>>,
+    }
+    impl<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a> Trait<'a, 'b>
+        for TraitMock<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>
+    {
+        fn fooo(&mut self, arg_1: Foo, qq: &'d mut &'b mut &'a &'a &'c &'d mut i32) {
+            let call = unsafe {
+                fooo_Call {
+                    _phantom_lifetime: PhantomData,
+                    _phantom_a: PhantomData,
+                    _phantom_b: PhantomData,
+                    arg_1: std::mem::transmute(arg_1),
+                    qq: std::mem::transmute(qq),
+                }
+            };
+            self.data.fooo_data.handle_base(&self, call);
+        }
+    }
+    impl<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a>
+        TraitMock<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>
+    {
+        #[allow(unused)]
+        pub fn new() -> Self {
+            let data = Arc::new(TraitMockData {
+                _phantom_lifetime: PhantomData,
+                fooo_data: FnData::new("fooo", &SERVICES),
+            });
+            return TraitMock {
+                setup: TraitMockSetup { data: data.clone() },
+                received: TraitMockReceived { data: data.clone() },
+                data,
+            };
+        }
+    }
+    impl<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a>
+        TraitMockSetup<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>
+    {
+        #[allow(unused)]
+        pub fn fooo(
+            &'__rsubstitute_arg_field_lifetime self,
+            arg_1: impl Into<Arg<Foo>>,
+            qq: impl Into<Arg<&'d mut &'b mut &'a &'a &'c &'d mut i32>>,
+        ) -> SharedFnConfig<
+            '__rsubstitute_arg_field_lifetime,
+            TraitMock<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+            fooo_Call<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+            fooo_ArgsChecker<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>,
+            (),
+            Self,
+        > {
+            let fooo_args_checker = fooo_ArgsChecker {
+                _phantom_lifetime: PhantomData,
+                _phantom_a: PhantomData,
+                _phantom_b: PhantomData,
+                arg_1: arg_1.into(),
+                qq: qq.into(),
+            };
+            let fn_config = self.data.fooo_data.add_config(fooo_args_checker);
+            let shared_fn_config = SharedFnConfig::new(fn_config, self);
+            return shared_fn_config;
+        }
+    }
+    impl<'__rsubstitute_arg_field_lifetime: 'a + 'b, 'a, 'b, 'c: 'a, 'd: 'b + 'a>
+        TraitMockReceived<'__rsubstitute_arg_field_lifetime, 'a, 'b, 'c, 'd>
+    {
+        #[allow(unused)]
+        pub fn fooo(
+            &'__rsubstitute_arg_field_lifetime self,
+            arg_1: impl Into<Arg<Foo>>,
+            qq: impl Into<Arg<&'d mut &'b mut &'a &'a &'c &'d mut i32>>,
+            times: Times,
+        ) -> &'__rsubstitute_arg_field_lifetime Self {
+            let fooo_args_checker = fooo_ArgsChecker {
+                _phantom_lifetime: PhantomData,
+                _phantom_a: PhantomData,
+                _phantom_b: PhantomData,
+                arg_1: arg_1.into(),
+                qq: qq.into(),
+            };
+            self.data
+                .fooo_data
+                .verify_received(fooo_args_checker, times);
+            return self;
+        }
+        pub fn no_other_calls(&'__rsubstitute_arg_field_lifetime self) {
+            self.data.verify_received_nothing_else();
+        }
     }
 }
 
--- src/rsubstitute_proc_macro/src/syntax/generics_merger.rs
+++ src/rsubstitute_proc_macro/src/syntax/generics_merger.rs
@@ -1,30 +1,88 @@
-use syn::{Generics, WhereClause};
+use syn::*;
 
 pub trait IGenericsMerger {
-    fn merge(&self, base: &mut Generics, merged: &Generics);
+    fn merge(&self, base_generics: &mut Generics, merged_generics: &Generics);
 }
 
 pub(crate) struct GenericsMerger;
 
 impl IGenericsMerger for GenericsMerger {
     // TODO - properly merge same type params and where clauses
-    fn merge(&self, base: &mut Generics, merged: &Generics) {
-        let where_clause =
-            self.merge_where_clause(base.where_clause.clone(), merged.where_clause.clone());
-        base.params
-        
-        let params = base
+    fn merge(&self, base_generics: &mut Generics, merged_generics: &Generics) {
+        let mut extra_params = Vec::with_capacity(merged_generics.params.len());
+        for merge_param in merged_generics.params.iter() {
+            let maybe_existing_param = self.try_find_existing_param(base_generics, &merge_param);
+
+            if let Some(existing_param) = maybe_existing_param {
+                match existing_param {
+                    ExistingParamPair::Lifetime { base, merge } => base.bounds.extend(merge.bounds),
+                    ExistingParamPair::Type { .. } => {}
+                    ExistingParamPair::Const { .. } => {}
+                }
+            }
+        }
+
+        base_generics.params.extend(extra_params);
+
+        let where_clause = self.merge_where_clause(
+            base_generics.where_clause.clone(),
+            merged_generics.where_clause.clone(),
+        );
+        let params = base_generics
             .params
             .clone()
             .into_iter()
-            .chain(merged.params.clone().into_iter())
+            .chain(merged_generics.params.clone().into_iter())
             .collect();
-        base.params = params;
-        base.where_clause = where_clause;
+        base_generics.params = params;
+        base_generics.where_clause = where_clause;
     }
 }
 
 impl GenericsMerger {
+    #[inline(always)]
+    fn try_find_existing_param<'a>(
+        &self,
+        base_generics: &'a mut Generics,
+        merge_param: &'a GenericParam,
+    ) -> Option<ExistingParamPair<'a>> {
+        let maybe_existing = match merge_param {
+            GenericParam::Lifetime(merge) => base_generics
+                .params
+                .iter_mut()
+                .find_map(|base_param| match base_param {
+                    GenericParam::Lifetime(base_lifetime)
+                        if merge.lifetime.ident == base_lifetime.lifetime.ident =>
+                    {
+                        Some(base_lifetime)
+                    }
+                    _ => None,
+                })
+                .map(|base| ExistingParamPair::Lifetime { base, merge }),
+            GenericParam::Type(merge) => base_generics
+                .params
+                .iter_mut()
+                .find_map(|base_param| match base_param {
+                    GenericParam::Type(base_type) if merge.ident == base_type.ident => {
+                        Some(base_type)
+                    }
+                    _ => None,
+                })
+                .map(|base| ExistingParamPair::Type { base, merge }),
+            GenericParam::Const(merge) => base_generics
+                .params
+                .iter_mut()
+                .find_map(|base_param| match base_param {
+                    GenericParam::Const(base_const) if merge.ident == base_const.ident => {
+                        Some(base_const)
+                    }
+                    _ => None,
+                })
+                .map(|base| ExistingParamPair::Const { base, merge }),
+        };
+        return maybe_existing;
+    }
+
     fn merge_where_clause(
         &self,
         maybe_first: Option<WhereClause>,
@@ -45,4 +103,19 @@ impl GenericsMerger {
             }
         }
     }
+}
+
+enum ExistingParamPair<'a> {
+    Lifetime {
+        base: &'a mut LifetimeParam,
+        merge: &'a LifetimeParam,
+    },
+    Type {
+        base: &'a mut TypeParam,
+        merge: &'a TypeParam,
+    },
+    Const {
+        base: &'a mut ConstParam,
+        merge: &'a ConstParam,
+    },
 }
