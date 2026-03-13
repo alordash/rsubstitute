a4ba77902917e62dcfa09c64a028c8f0b6fa448e
alordash <alordash@mail.ru>
2026-03-14 03:37:56 +0400

    Added static fn and struct to eloquent reference test

--- src/rsubstitute/tests/reference_tests.rs
+++ src/rsubstitute/tests/reference_tests.rs
@@ -1,4 +1,4 @@
-use rsubstitute_proc_macro::mock;
+use rsubstitute_proc_macro::{mock, mocked};
 use std::marker::PhantomData;
 
 #[derive(Default, Debug)]
@@ -32,6 +32,83 @@ trait Trait<'a, 'b: 'a> {
             &&'a &&'b &[&'c &&'b &Data<'c, 'a, &&&'c &i32, Vec<&'d &'b &()>>],
         >,
     );
+}
+
+#[mock]
+fn work<'a, 'b: 'a, 'c, 'd: 'a>(
+    a: &'a i32,
+    b: &'b i32,
+    c: &'c i32,
+    d: &'d i32,
+    axb: &'a &&'b i32,
+    cxd: &'c &&'d i32,
+    abxbax: &'a &'b &&'b &'a &i32,
+    cdxdcx: &'c &'d &&'d &'c &i32,
+    abcd: &'a &'b &'c &'d i32,
+    xaxbxcxdx: &&'a &&'b &&'c &&'d &i32,
+    data: Data<'a, 'b, &&i32, &&'a &&'b &[&'c &&'b &Data<'c, 'a, &&&'c &i32, Vec<&'d &'b &()>>]>,
+) {
+}
+
+mocked! {
+    struct Struct<'a, 'b: 'a> {
+        _phantom_a: PhantomData<&'a ()>,
+        _phantom_b: PhantomData<&'b ()>,
+    }
+
+    impl<'a, 'b: 'a> Struct<'a, 'b> {
+        pub fn new() -> Self {
+            Self {
+                _phantom_a: PhantomData,
+                _phantom_b: PhantomData,
+            }
+        }
+
+        fn work<'c, 'd: 'a>(
+            &self,
+            a: &'a i32,
+            b: &'b i32,
+            c: &'c i32,
+            d: &'d i32,
+            axb: &'a &&'b i32,
+            cxd: &'c &&'d i32,
+            abxbax: &'a &'b &&'b &'a &i32,
+            cdxdcx: &'c &'d &&'d &'c &i32,
+            abcd: &'a &'b &'c &'d i32,
+            xaxbxcxdx: &&'a &&'b &&'c &&'d &i32,
+            data: Data<
+                'a,
+                'b,
+                &&i32,
+                &&'a &&'b &[&'c &&'b &Data<'c, 'a, &&&'c &i32, Vec<&'d &'b &()>>],
+            >,
+        ) {
+        }
+    }
+
+    impl<'a, 'b: 'a> Trait<'a, 'b> for Struct<'a, 'b> {
+        fn work<'c, 'd: 'a>(
+            &self,
+            a: &'a i32,
+            b: &'b i32,
+            c: &'c i32,
+            d: &'d i32,
+            axb: &'a &&'b i32,
+            cxd: &'c &&'d i32,
+            abxbax: &'a &'b &&'b &'a &i32,
+            cdxdcx: &'c &'d &&'d &'c &i32,
+            abcd: &'a &'b &'c &'d i32,
+            xaxbxcxdx: &&'a &&'b &&'c &&'d &i32,
+            data: Data<
+                'a,
+                'b,
+                &&i32,
+                &&'a &&'b &[&'c &&'b &Data<'c, 'a, &&&'c &i32, Vec<&'d &'b &()>>],
+            >,
+        ) {
+            Struct::work(self, a, b, c, d, axb, cxd, abxbax, cdxdcx, abcd, xaxbxcxdx, data);
+        }
+    }
 }
 
 #[cfg(test)]
