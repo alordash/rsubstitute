d5cadde036b050530fe5549823488484e72085ff
alordash <alordash@mail.ru>
2026-03-28 01:19:19 +0400

    Adding test case for generics merging

--- src/rsubstitute/tests/struct_repeating_generics_tests.rs
+++ src/rsubstitute/tests/struct_repeating_generics_tests.rs
@@ -7,22 +7,46 @@ macro_rules! define_marker_traits {
 
 define_marker_traits!(M1, M2, M3, M4);
 
+trait ILifetime<'x: 'c, 'a, 'b, 'c, 'd>
+where
+    'x: 'd,
+{
+}
 
+trait IType<T: M3>
+where
+    T: M4,
+{
+}
 
-struct Lifetime<'x: 'a, 'a, 'b>(PhantomData<&'x &'a &'b ()>);
-struct Type<T: M1>(PhantomData<T>) where T: M2;
-struct Const<const N: usize>;
+trait IConst<const C: usize> {}
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn compile() {
-//         // let mock = Struct::<i32, String>::new();
-//         //
-//         // Same::work(&mock, &3);
-//         //
-//         // mock.received.as_Same.work(&3, Times::Once);
-//     }
-// }
+mocked_base! {
+    struct Lifetime<'x: 'a, 'a, 'b, 'c, 'd>(PhantomData<&'x &'a &'b &'c &'d ()>)
+    where
+        'x: 'b;
+
+    impl<'x: 'a + 'c, 'a, 'b, 'c, 'd> Lifetime<'x, 'a, 'b, 'c, 'd > where 'x: 'b + 'd  {
+        pub fn new() -> Self {
+            Self(PhantomData)
+        }
+    }
+
+    impl<'x: 'a + 'c, 'a, 'b, 'c, 'd> ILifetime<'x, 'a, 'b, 'c, 'd> for Lifetime<'x, 'a, 'b, 'c, 'd>
+    where
+        'x: 'b + 'd
+    {}
+}
+
+struct Type<T: M1>(PhantomData<T>)
+where
+    T: M2;
+struct Const<const C: usize>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn compile() {}
+}
