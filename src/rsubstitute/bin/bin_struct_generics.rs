a1ff6cf56764aedcf602f1eb0a70a3a055af5f91
alordash <alordash1@gmail.com>
2026-03-28 17:50:48 +0400

    Bin test cases

--- src/rsubstitute/bin/bin_struct_generics.rs
+++ src/rsubstitute/bin/bin_struct_generics.rs
@@ -1,19 +1,21 @@
 use std::marker::PhantomData;
 use rsubstitute_proc_macro::mocked_base;
 
-mocked_base! {
+// mocked_base! {
     struct Struct<T>(PhantomData<T>);
     
     impl Struct<i32> {
         pub fn new() -> Self {
             Self { 0: PhantomData }
         }
+        
+        pub fn flex(&self) {}
     }
     
     impl Struct<f32> {
         pub fn flex(&self) {}
     }
-}
+// }
 
 fn main() {
     println!("Done");
