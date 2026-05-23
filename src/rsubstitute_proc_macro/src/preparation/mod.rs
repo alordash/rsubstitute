ac1fc79d1133befb7bbb77cca59736c5e08820eb
alordash <alordash@mail.ru>
2026-05-24 03:15:03 +0400

    struct base

--- src/rsubstitute_proc_macro/src/preparation/mod.rs
+++ src/rsubstitute_proc_macro/src/preparation/mod.rs
@@ -1,6 +1,7 @@
 // TODO - add `trait` and `use`
 pub mod r#fn;
 pub mod r#trait;
+pub mod r#struct;
 
 mod context_creation;
 
---
+++ src/rsubstitute_proc_macro/src/preparation/struct/mod.rs
@@ -0,0 +1,3 @@
+mod impl_struct_syntax_preparation;
+
+pub use impl_struct_syntax_preparation::*;
