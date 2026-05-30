af0d833beede3db50ef6d090d61c833398493e5c
alordash <alordash@mail.ru>
2026-05-31 03:37:20 +0400

    better error msg

--- src/rsubstitute_proc_macro/src/syntax/type.rs
+++ src/rsubstitute_proc_macro/src/syntax/type.rs
@@ -1,3 +1,4 @@
+use quote::ToTokens;
 use syn::*;
 
 pub mod path;
@@ -5,7 +6,10 @@ pub mod path;
 pub(crate) fn to_ident(ty: &Type) -> Ident {
     let result = match ty {
         Type::Path(type_path) => path::to_ident(type_path),
-        unsupported => panic!("Only path types supported."),
+        unsupported => panic!(
+            "Only path types supported. Received: {}",
+            unsupported.to_token_stream().to_string()
+        ),
     };
     return result;
 }
