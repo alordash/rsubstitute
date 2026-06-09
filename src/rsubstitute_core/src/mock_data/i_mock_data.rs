839123695d653c8ec168f7b2ff7531d62a1f911f
alordash <alordash@mail.ru>
2026-06-10 01:55:57 +0400

    opt

--- src/rsubstitute_core/src/mock_data/i_mock_data.rs
+++ src/rsubstitute_core/src/mock_data/i_mock_data.rs
@@ -5,10 +5,10 @@ pub trait IMockData {
 
     fn verify_received_nothing_else(&self) {
         let all_error_msgs: Vec<_> = self.get_received_nothing_else_error_msgs();
-        let error_msgs: Vec<_> = all_error_msgs.into_iter().flatten().collect();
-        if error_msgs.is_empty() {
+        if all_error_msgs.is_empty() {
             return;
         }
+        let error_msgs: Vec<_> = all_error_msgs.into_iter().flatten().collect();
         error_printing::panic_received_unexpected_calls_error(error_msgs);
     }
 }
