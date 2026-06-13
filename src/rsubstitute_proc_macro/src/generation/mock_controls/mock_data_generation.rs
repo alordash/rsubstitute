68e1072f6bba0be381be28053fcb10e54b91cf17
alordash <alordash1@gmail.com>
2026-06-13 16:34:46 +0200

    Mock data impl generation

--- src/rsubstitute_proc_macro/src/generation/mock_controls/mock_data_generation.rs
+++ src/rsubstitute_proc_macro/src/generation/mock_controls/mock_data_generation.rs
@@ -1,6 +1,9 @@
+mod mock_data_impl_generation;
+
 use crate::generation::mock_controls::models::*;
 use crate::generation::r#fn::models::*;
-use crate::syntax::{r#type, static_lifetime};
+use crate::syntax::*;
+use mock_data_impl_generation::*;
 use proc_macro2::Span;
 use quote::format_ident;
 use syn::*;
@@ -32,7 +35,7 @@ pub(crate) fn generate_mock_data(
     };
 
     let r#type = Type::Path(r#type::path::from_ident(item_struct.ident.clone()));
-    let mock_data_impl = todo!();
+    let mock_data_impl = generate_mock_data_impl(span, fn_infos, r#type.clone());
 
     let result = MockDataStruct {
         r#type,
---
+++ src/rsubstitute_proc_macro/src/generation/mock_controls/mock_data_generation/mock_data_impl_generation.rs
@@ -0,0 +1,90 @@
+use crate::generation::r#fn::models::*;
+use crate::syntax::*;
+use proc_macro2::Span;
+use quote::ToTokens;
+use syn::punctuated::Punctuated;
+use syn::*;
+
+pub(crate) fn generate_mock_data_impl(
+    span: Span,
+    fn_infos: &[FnInfo],
+    target_type: Type,
+) -> ItemImpl {
+    let fn_get_received_nothing_else_error_msgs =
+        generate_fn_get_received_nothing_else_error_msgs(span, fn_infos);
+
+    let items = vec![ImplItem::Fn(fn_get_received_nothing_else_error_msgs)];
+
+    let result = ItemImpl {
+        attrs: Vec::new(),
+        defaultness: None,
+        unsafety: None,
+        impl_token: Token![impl](span),
+        generics: Generics::default(),
+        trait_: Some((None, path::new(span, ["IMockData"]), Token![for](span))),
+        self_ty: Box::new(target_type),
+        brace_token: token::Brace(span),
+        items,
+    };
+
+    return result;
+}
+
+fn generate_fn_get_received_nothing_else_error_msgs(span: Span, fn_infos: &[FnInfo]) -> ImplItemFn {
+    let sig = Signature {
+        constness: None,
+        asyncness: None,
+        unsafety: None,
+        abi: None,
+        fn_token: Token![fn](span),
+        ident: Ident::new("get_received_nothing_else_error_msgs", span),
+        generics: Generics::default(),
+        paren_token: token::Paren(span),
+        inputs: [ref_self_fn_arg(span)].into_iter().collect(),
+        variadic: None,
+        output: ReturnType::Type(
+            Token!(->)(span),
+            Box::new(Type::Path(r#type::vec_of(
+                span,
+                Type::Path(r#type::vec_of(
+                    span,
+                    Type::Path(r#type::path::new(span, ["String"])),
+                )),
+            ))),
+        ),
+    };
+
+    let vec_exprs: Punctuated<Expr, Token![,]> = fn_infos
+        .iter()
+        .map(|fn_info| {
+            let span = fn_info.syntax.spans.inputs;
+            let result = expr::method_call::new(
+                span,
+                Expr::Field(expr::field::new_self(fn_info.syntax.fn_ident.clone())),
+                Ident::new("get_unexpected_calls_error_msgs", span),
+                [],
+            );
+
+            return Expr::MethodCall(result);
+        })
+        .collect();
+    let vec_expr = ExprMacro {
+        attrs: Vec::new(),
+        mac: r#macro::vec(span, vec_exprs.to_token_stream()),
+    };
+
+    let block = Block {
+        brace_token: token::Brace(span),
+        stmts: vec![Stmt::Expr(Expr::Macro(vec_expr), None)],
+    };
+
+    let result = ImplItemFn {
+        attrs: Vec::new(),
+        vis: Visibility::Inherited,
+        defaultness: None,
+        sig,
+        block,
+    };
+
+    return result;
+}
