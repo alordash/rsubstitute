e127c54ca9e999ba991f64bbd37e540f8f12c155
alordash <alordash@mail.ru>
2026-06-17 02:11:24 +0200

    generated fields last

--- src/rsubstitute_proc_macro/src/generation/mock_controls/mock.rs
+++ src/rsubstitute_proc_macro/src/generation/mock_controls/mock.rs
@@ -11,38 +11,37 @@ pub(crate) fn generate(
     setup_struct_ident: Ident,
     received_struct_ident: Ident,
     data_struct_ident: Ident,
-    base_fields: impl IntoIterator<Item = Field>,
+    base_fields: impl Iterator<Item = Field>,
 ) -> Mock {
     let path = path::from_ident(mock_ident.clone());
 
-    let named_fields = [
-        Field {
-            attrs: Vec::new(),
-            vis: Visibility::Public(Token!(pub)(source_span)),
-            mutability: FieldMutability::None,
-            ident: Some(setup_ident(source_span)),
-            colon_token: Some(Token![:](source_span)),
-            ty: Type::Path(TypePath {
-                qself: None,
-                path: path::from_ident(setup_struct_ident),
-            }),
-        },
-        Field {
-            attrs: Vec::new(),
-            vis: Visibility::Public(Token!(pub)(source_span)),
-            mutability: FieldMutability::None,
-            ident: Some(received_ident(source_span)),
-            colon_token: Some(Token![:](source_span)),
-            ty: Type::Path(TypePath {
-                qself: None,
-                path: path::from_ident(received_struct_ident),
-            }),
-        },
-        arc_data_field::new(source_span, data_struct_ident),
-    ]
-    .into_iter()
-    .chain(base_fields)
-    .collect();
+    let named_fields = base_fields
+        .chain([
+            Field {
+                attrs: Vec::new(),
+                vis: Visibility::Public(Token!(pub)(source_span)),
+                mutability: FieldMutability::None,
+                ident: Some(setup_ident(source_span)),
+                colon_token: Some(Token![:](source_span)),
+                ty: Type::Path(TypePath {
+                    qself: None,
+                    path: path::from_ident(setup_struct_ident),
+                }),
+            },
+            Field {
+                attrs: Vec::new(),
+                vis: Visibility::Public(Token!(pub)(source_span)),
+                mutability: FieldMutability::None,
+                ident: Some(received_ident(source_span)),
+                colon_token: Some(Token![:](source_span)),
+                ty: Type::Path(TypePath {
+                    qself: None,
+                    path: path::from_ident(received_struct_ident),
+                }),
+            },
+            arc_data_field::new(source_span, data_struct_ident),
+        ])
+        .collect();
 
     let item_struct = ItemStruct {
         attrs: Vec::new(),
