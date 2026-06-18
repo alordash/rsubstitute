eba82d5dd279bcd205db68722077583ef1a5a0d8
alordash <alordash@mail.ru>
2026-06-18 02:52:41 +0200

    DerefMut, struct mock usage

--- src/rsubstitute_core/src/infrastructure/i_mock.rs
+++ src/rsubstitute_core/src/infrastructure/i_mock.rs
@@ -1,11 +1,29 @@
-pub trait IMock<TMock>: Sized {
-    fn get_boxed_mocked(&mut self) -> &mut TMock;
+use std::ops::DerefMut;
 
+pub(crate) struct InternalMarker;
+
+pub trait IMock<TMock>: Sized + DerefMut<Target = TMock> {
     fn drop_boxed_mocked(&mut self) {
-        let mocked = self.get_boxed_mocked();
+        let mocked = self.deref_mut();
         // SAFETY: this frees memory leaked from `IMocked::mock` method.
         unsafe {
             let _ = Box::from_raw(mocked as *mut _);
         }
     }
+}
+
+struct S<'a> {
+    pub r: &'a Vec<i32>,
+}
+
+fn flex(s: S) -> (&Vec<i32>, S) {
+    (s.r, s)
+}
+
+fn doo() {
+    let s = S { r: &vec![1, 2, 3] };
+    let (r, s) = flex(s);
+    let v1 = r[1];
+    drop(s);
+    let v2 = r[1];
 }
--- src/rsubstitute_core/src/infrastructure/i_mockable.rs
+++ src/rsubstitute_core/src/infrastructure/i_mockable.rs
@@ -1,17 +1,18 @@
-use crate::infrastructure::IMock;
+use crate::infrastructure::*;
 use crate::transmute_lifetime;
+use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 
 pub trait IMocked<TMock>: Sized {
     fn mock_from_ref(&mut self) -> TMock;
 
-    fn mock<'a>(self) -> (&'a mut Self, TMock) {
+    fn mocked<'a>(self) -> TMock {
         let mock_box = Box::new(self);
         // LEAK: mock is leaked because it will be cleared by
         // `TMock`'s `Drop` implementation using `IMock::drop_boxed_mocked`.
         let mock_leaked = Box::leak(mock_box);
         let mock = mock_leaked.mock_from_ref();
-        return (mock_leaked, mock);
+        return mock;
     }
 }
 
@@ -21,25 +22,49 @@ struct Struct<'b> {
 }
 
 struct StructMock<'a, 'b> {
-    pub mocked: &'a mut Struct<'b>,
+    __rs_mocked: &'a mut Struct<'b>,
 }
 
 impl<'a, 'b> IMocked<StructMock<'a, 'b>> for Struct<'b> {
     fn mock_from_ref(&mut self) -> StructMock<'a, 'b> {
         StructMock {
-            mocked: transmute_lifetime!(self),
+            __rs_mocked: transmute_lifetime!(self),
         }
     }
 }
 
-impl<'a, 'b> IMock<Struct<'b>> for StructMock<'a, 'b> {
-    fn get_boxed_mocked(&mut self) -> &mut Struct<'b> {
-        self.mocked
+impl<'a, 'b> Deref for StructMock<'a, 'b> {
+    type Target = Struct<'b>;
+
+    fn deref(&self) -> &Self::Target {
+        self.__rs_mocked
     }
 }
+
+impl<'a, 'b> DerefMut for StructMock<'a, 'b> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.__rs_mocked
+    }
+}
+
+impl<'a, 'b> IMock<Struct<'b>> for StructMock<'a, 'b> {}
 
 impl<'a, 'b> Drop for StructMock<'a, 'b> {
     fn drop(&mut self) {
         self.drop_boxed_mocked()
     }
+}
+
+fn usage(rv: &[u8; 3]) {
+    let s = Struct {
+        data: Arc::new(vec![5, 3, 2]),
+        r: rv,
+    };
+    let mocked = s.mocked();
+    let data = &mocked.data;
+    let r = &mocked.r;
+    dbg!(data, r);
+    drop(mocked);
+    dbg!(data);
+    dbg!(r);
 }
