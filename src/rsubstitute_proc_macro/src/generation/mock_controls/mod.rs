c995e2d394faaf353d24ace143830d7babc0c2df
alordash <alordash@mail.ru>
2026-06-29 02:26:42 +0200

    args_checker_stmt

--- src/rsubstitute_proc_macro/src/generation/mock_controls/mod.rs
+++ src/rsubstitute_proc_macro/src/generation/mock_controls/mod.rs
@@ -1,7 +1,13 @@
 pub mod models {
     mod static_setup;
-    
+
     pub use static_setup::*;
 }
+
+mod common {
+    pub(crate) mod args_checker_stmt;
+}
+
+use common::*; 
 
 pub mod static_setup;
--- src/rsubstitute_proc_macro/src/generation/mock_controls/static_setup.rs
+++ src/rsubstitute_proc_macro/src/generation/mock_controls/static_setup.rs
@@ -2,6 +2,7 @@ use crate::common::models::*;
 use crate::common::*;
 use crate::generation::fn_info::models::*;
 use crate::generation::mock_controls::models::*;
+use crate::generation::mock_controls::*;
 use crate::syntax::*;
 use proc_macro2::Span;
 use quote::format_ident;
@@ -214,45 +215,7 @@ fn generate_setup_fn(ctx: &Context, span: Span, mock_path: &Path, fn_info: &FnIn
         semi_token: Token![;](span),
     };
 
-    let args_checker_var_path = expr::path::new(span, ["args_checker"]);
-    let args_checker_stmt = Local {
-        attrs: Vec::new(),
-        let_token: Token![let](span),
-        pat: Pat::Path(args_checker_var_path.clone()),
-        init: Some(LocalInit {
-            eq_token: Token![=](span),
-            expr: Box::new(Expr::Struct(ExprStruct {
-                attrs: Vec::new(),
-                qself: None,
-                path: fn_info.args_checker_struct.path.clone(),
-                brace_token: token::Brace(span),
-                fields: [generics_field::new_value(span)]
-                    .into_iter()
-                    .chain(fn_info.syntax.arguments.iter().map(|x| FieldValue {
-                        attrs: Vec::new(),
-                        member: Member::Named(x.ident.clone()),
-                        colon_token: Some(Token![:](span)),
-                        expr: Expr::Macro(transmute_lifetime_expr::new(Expr::MethodCall(
-                            expr::method_call::new(
-                                span,
-                                Expr::Path(ExprPath {
-                                    attrs: Vec::new(),
-                                    qself: None,
-                                    path: path::from_ident(x.ident.clone()),
-                                }),
-                                Ident::new("into", span),
-                                [],
-                            ),
-                        ))),
-                    }))
-                    .collect(),
-                dot2_token: None,
-                rest: None,
-            })),
-            diverge: None,
-        }),
-        semi_token: Token![;](span),
-    };
+    let (args_checker_var_path, args_checker_stmt) = args_checker_stmt::new(span, fn_info);
     let fn_configurator_var_path = expr::path::new(span, ["fn_configurator"]);
     let fn_configurator_stmt = Local {
         attrs: Vec::new(),
