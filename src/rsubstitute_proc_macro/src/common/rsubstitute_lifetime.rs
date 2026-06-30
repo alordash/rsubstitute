920d421cd22d87f36063e5426d2e7a3dcfc29981
alordash <alordash@mail.ru>
2026-06-30 02:12:03 +0200

    fix lifetime name

--- src/rsubstitute_proc_macro/src/common/rsubstitute_lifetime.rs
+++ src/rsubstitute_proc_macro/src/common/rsubstitute_lifetime.rs
@@ -2,7 +2,7 @@ use proc_macro2::Span;
 use syn::*;
 
 pub(crate) fn new(span: Span) -> Lifetime {
-    let result = Lifetime::new("__rsa", span);
+    let result = Lifetime::new("'__rsa", span);
 
     return result;
 }
