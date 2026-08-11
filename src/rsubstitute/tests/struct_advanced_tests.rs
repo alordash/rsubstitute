618ffd1a14ca1c6ba36a797a3a3702ddc6b887a8
alordash <alordash1@gmail.com>
2026-08-11 18:33:18 +0400

    limited support test

--- src/rsubstitute/tests/struct_advanced_tests.rs
+++ src/rsubstitute/tests/struct_advanced_tests.rs
@@ -10,8 +10,25 @@ struct Struct;
 
 #[mock(base)]
 impl Struct {
+    fn new() -> Self {
+        Self
+    }
+
     fn fooo(&mut self, Foo { mut number }: Foo, mut qq: &mut &mut &&&&mut i32) {
         println!("number: {number:?}")
+    }
+}
+
+#[mock(base)]
+impl From<i32> for Struct {
+    fn from(_: i32) -> Self {
+        Self::new()
+    }
+}
+
+impl From<f32> for Struct {
+    fn from(_: f32) -> Self {
+        Self::new()
     }
 }
 
