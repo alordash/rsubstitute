3a253b4b1a7c074fdd608ddfb5a9f022f5aef4da
alordash <alordash1@gmail.com>
2026-08-11 16:39:33 +0400

    #[cfg_attr(test, mock)] for rsubstitutifier, multiple impls test

--- src/rsubstitutifier/src/mock_attribute.rs
+++ src/rsubstitutifier/src/mock_attribute.rs
@@ -1,13 +1,12 @@
 use proc_macro2::Span;
 use quote::ToTokens;
+use syn::punctuated::Punctuated;
 use syn::*;
 
 pub fn new(span: Span) -> Attribute {
-    Attribute {
-        pound_token: Default::default(),
-        style: AttrStyle::Outer,
-        bracket_token: Default::default(),
-        meta: Meta::Path(Path {
+    return wrap_in_cfg_attr(
+        span,
+        Meta::Path(Path {
             leading_colon: None,
             segments: [
                 PathSegment {
@@ -22,15 +21,13 @@ pub fn new(span: Span) -> Attribute {
             .into_iter()
             .collect(),
         }),
-    }
+    );
 }
 
 pub fn new_base(span: Span) -> Attribute {
-    Attribute {
-        pound_token: Default::default(),
-        style: AttrStyle::Outer,
-        bracket_token: Default::default(),
-        meta: Meta::List(MetaList {
+    wrap_in_cfg_attr(
+        span,
+        Meta::List(MetaList {
             path: Path {
                 leading_colon: None,
                 segments: [
@@ -49,5 +46,41 @@ pub fn new_base(span: Span) -> Attribute {
             delimiter: MacroDelimiter::Paren(Default::default()),
             tokens: Ident::new("base", span).to_token_stream(),
         }),
-    }
+    )
+}
+
+fn wrap_in_cfg_attr(span: Span, meta: Meta) -> Attribute {
+    let tokens: Punctuated<Meta, Token![,]> = [
+        Meta::Path(Path {
+            leading_colon: None,
+            segments: [PathSegment {
+                ident: Ident::new("test", span),
+                arguments: PathArguments::None,
+            }]
+            .into_iter()
+            .collect(),
+        }),
+        meta,
+    ]
+    .into_iter()
+    .collect();
+    let result = Attribute {
+        pound_token: Default::default(),
+        style: AttrStyle::Outer,
+        bracket_token: Default::default(),
+        meta: Meta::List(MetaList {
+            path: Path {
+                leading_colon: None,
+                segments: [PathSegment {
+                    ident: Ident::new("cfg_attr", span),
+                    arguments: PathArguments::None,
+                }]
+                .into_iter()
+                .collect(),
+            },
+            delimiter: MacroDelimiter::Paren(token::Paren(span)),
+            tokens: tokens.to_token_stream(),
+        }),
+    };
+    return result;
 }
