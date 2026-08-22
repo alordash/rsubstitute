3dd277bd38ccbc46be753ccf47ecf6a42ef06cd7
alordash <alordash1@gmail.com>
2026-08-22 19:34:53 +0400

    handle_async for the win

--- rsubstitute/bin/NEW_async.rs
+++ rsubstitute/bin/NEW_async.rs
@@ -1,4 +1,5 @@
-use rsubstitute::{mock, Arg, Mockable};
+use rsubstitute::{Arg, Mockable, mock};
+use rsubstitute_core::Times;
 use std::pin::Pin;
 use std::sync::atomic::{AtomicI32, Ordering};
 use std::sync::{Arc, Mutex};
@@ -6,7 +7,6 @@ use std::task::{Context, Poll, Waker};
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
-use rsubstitute_core::Times;
 
 #[mock]
 struct MyFuture {
@@ -42,10 +42,146 @@ impl MyFuture {
     }
 }
 
-#[mock(base)]
 async fn work() -> i32 {
-    tokio::time::sleep(Duration::from_secs(1));
-    return 12;
+    use work::*;
+    let call = work_Call::<'_> {
+        __rs_generics: ::core::marker::PhantomData,
+    };
+    let fn_data: &::rsubstitute::for_generated::FnData<'_, workMock<'_>, true, true, false> =
+        ::rsubstitute::for_generated::get_static_fn_data("work");
+    fn_data.handle_async((), call, __rs_base_work).await
+}
+#[allow(unused)]
+#[allow(unreachable_pub)]
+#[allow(nonstandard_style)]
+mod work {
+    use super::*;
+    #[doc(hidden)]
+    pub async fn __rs_base_work(_: (), call: work_Call<'_>) -> i32 {
+        let work_Call::<'_> { .. } = call;
+        tokio::time::sleep(Duration::from_secs(1));
+        return 12;
+    }
+    pub fn setup<'__rsa>() -> ::rsubstitute::for_generated::FnConfigurator<
+        '__rsa,
+        workMock<'__rsa>,
+        workStaticSetup<'__rsa>,
+        (),
+        i32,
+        workMock<'__rsa>,
+        true,
+        true,
+        false,
+    > {
+        ::rsubstitute::for_generated::clear_static_fn_data::<workMock<'__rsa>>();
+        workStaticSetup::<'__rsa> {
+            __rs_generics: ::core::marker::PhantomData,
+        }
+        .setup()
+    }
+    pub fn received<'__rsa>(
+        times: ::rsubstitute::for_generated::Times,
+    ) -> ::rsubstitute::for_generated::ArgRefsBinder<workStaticReceived<'__rsa>, ()> {
+        workStaticReceived::<'__rsa> {
+            __rs_generics: ::core::marker::PhantomData,
+        }
+        .received(times)
+    }
+    pub struct work_Call<'__rsa> {
+        pub __rs_generics: ::core::marker::PhantomData<(&'__rsa (),)>,
+    }
+    impl<'__rsa> ::rsubstitute::for_generated::IGenericsInfoProvider for work_Call<'__rsa> {}
+    impl<'__rsa> ::rsubstitute::for_generated::ICall for work_Call<'__rsa> {}
+    impl<'__rsa> ::core::clone::Clone for work_Call<'__rsa> {
+        #[inline]
+        fn clone(&self) -> work_Call<'__rsa> {
+            work_Call::<'__rsa> {
+                __rs_generics: ::core::clone::Clone::clone(&self.__rs_generics),
+            }
+        }
+    }
+    struct work_ArgsChecker<'__rsa> {
+        pub __rs_generics: ::core::marker::PhantomData<(&'__rsa (),)>,
+    }
+    impl<'__rsa> ::rsubstitute::for_generated::IGenericsInfoProvider for work_ArgsChecker<'__rsa> {}
+    impl<'__rsa> ::rsubstitute::for_generated::IArgsChecker for work_ArgsChecker<'__rsa> {}
+    pub struct workMock<'__rsa> {
+        pub __rs_generics: ::core::marker::PhantomData<(&'__rsa (),)>,
+    }
+    pub struct workStaticSetup<'__rsa> {
+        pub __rs_generics: ::core::marker::PhantomData<(&'__rsa (),)>,
+    }
+    impl<'__rsa> workStaticSetup<'__rsa> {
+        pub fn setup(
+            &self,
+        ) -> ::rsubstitute::for_generated::FnConfigurator<
+            '_,
+            workMock<'__rsa>,
+            Self,
+            (),
+            i32,
+            workMock<'__rsa>,
+            true,
+            true,
+            false,
+        > {
+            let args_checker = work_ArgsChecker::<'__rsa> {
+                __rs_generics: ::core::marker::PhantomData,
+            };
+            let fn_data: &::rsubstitute::for_generated::FnData<
+                '_,
+                workMock<'__rsa>,
+                true,
+                true,
+                false,
+            > = ::rsubstitute::for_generated::get_static_fn_data("work");
+            let fn_configurator: ::rsubstitute::for_generated::FnConfigurator<
+                '_,
+                workMock<'__rsa>,
+                Self,
+                (),
+                i32,
+                workMock<'__rsa>,
+                true,
+                true,
+                false,
+            > = fn_data.add_config(args_checker, self);
+            ::rsubstitute::transmute_lifetime!(fn_configurator)
+        }
+    }
+    pub struct workStaticReceived<'__rsa> {
+        pub __rs_generics: ::core::marker::PhantomData<(&'__rsa (),)>,
+    }
+    impl<'__rsa> ::core::clone::Clone for workStaticReceived<'__rsa> {
+        #[inline]
+        fn clone(&self) -> workStaticReceived<'__rsa> {
+            workStaticReceived::<'__rsa> {
+                __rs_generics: ::core::clone::Clone::clone(&self.__rs_generics),
+            }
+        }
+    }
+    impl<'__rsa> workStaticReceived<'__rsa> {
+        pub fn received(
+            &self,
+            times: ::rsubstitute::for_generated::Times,
+        ) -> ::rsubstitute::for_generated::ArgRefsBinder<Self, ()> {
+            let args_checker = work_ArgsChecker::<'__rsa> {
+                __rs_generics: ::core::marker::PhantomData,
+            };
+            let fn_data: &::rsubstitute::for_generated::FnData<
+                '_,
+                workMock<'_>,
+                true,
+                true,
+                false,
+            > = ::rsubstitute::for_generated::get_static_fn_data("work");
+            fn_data.verify_received(args_checker, times);
+            rsubstitute::for_generated::ArgRefsBinder::new(self.clone())
+        }
+        pub fn no_other_calls(&self) {
+            ::rsubstitute::for_generated::verify_static_fn_received_nothing_else::<workMock<'_>>()
+        }
+    }
 }
 
 #[mock(base)]
@@ -69,16 +205,16 @@ async fn main() {
     future.setup().as_Future().poll(Arg::Any).call_base();
     let result = future.await;
     assert_eq!(166, result);
-    
+
     work::setup().returns(515);
     let work_result = work().await;
     assert_eq!(515, work_result);
     work::received(Times::Once).no_other_calls();
     dbg!(work_result);
 
-    // work::setup().call_base();
-    // let work_result = work().await;
-    // assert_eq!(12, work_result);
-    // work::received(Times::Exactly(2)).no_other_calls();
-    // dbg!(work_result);
+    work::setup().call_base();
+    let work_result = work().await;
+    assert_eq!(12, work_result);
+    work::received(Times::Once).no_other_calls();
+    dbg!(work_result);
 }
--- rsubstitute_core/src/infrastructure/fn_data.rs
+++ rsubstitute_core/src/infrastructure/fn_data.rs
@@ -257,6 +257,58 @@ impl<'rs, TMock, const PASSES_MOCK_TO_CALLBACK: bool>
         };
         return return_value.downcast_into();
     }
+    pub async fn handle_async<
+        'a,
+        TMockArg,
+        TCall: ICall + Clone,
+        TReturnValue: IReturnValue<'a>,
+        TBaseCall: FnMut(TMockArg, TCall) -> Fut,
+        Fut: Future<Output = TReturnValue>,
+    >(
+        &self,
+        mock_arg: TMockArg,
+        the_call: TCall,
+        mut base_call: TBaseCall,
+    ) -> TReturnValue {
+        let call_for_base_call = the_call.clone();
+        let dyn_call = DynCall::new(the_call);
+        let call = Rc::new(dyn_call);
+        let with_return_value = true;
+        let fn_config = match self.try_get_matching_config(&call, with_return_value) {
+            MatchingConfigSearchResult::Ok(x) => x,
+            MatchingConfigSearchResult::Err(matching_config_search_err) => {
+                if self.force_call_base {
+                    let base_return_value = base_call(mock_arg, call_for_base_call);
+                    return base_return_value.await;
+                }
+                error_printing::panic_no_suitable_fn_configuration_found(
+                    self.fn_name,
+                    call.get_arg_infos(),
+                    call.get_generic_parameter_infos(),
+                    matching_config_search_err,
+                )
+            }
+        };
+        self.register_call(call.clone());
+        fn_config.borrow_mut().register_call(call.clone());
+        let fn_config_ref = fn_config.borrow();
+        if let Some(callback) = fn_config_ref.get_callback() {
+            callback.borrow_mut()(&mock_arg as *const TMockArg as *const (), call.as_ref());
+        }
+        if fn_config_ref.should_call_base() {
+            let base_return_value = base_call(mock_arg, call_for_base_call);
+            return base_return_value.await;
+        }
+        drop(fn_config_ref);
+        let Some(return_value) = fn_config.borrow_mut().select_next_return_value(&call) else {
+            error_printing::panic_no_return_value_was_configured(
+                self.fn_name,
+                call.get_arg_infos(),
+                call.get_generic_parameter_infos(),
+            );
+        };
+        return return_value.downcast_into();
+    }
 }
 
 // For static fns
