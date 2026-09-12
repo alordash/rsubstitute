c4b34636222899da9140c8f2f419a63326f747db
alordash <alordash1@gmail.com>
2026-09-12 17:27:01 +0400

    remove duplicate clippy allows

--- rsubstitute_proc_macro/src/lib.rs
+++ rsubstitute_proc_macro/src/lib.rs
@@ -1,6 +1,3 @@
-#![allow(clippy::needless_return)]
-#![allow(clippy::match_like_matches_macro)]
-
 use crate::common::context;
 use crate::generation::targets;
 use crate::generation::targets::models::MockMod;
